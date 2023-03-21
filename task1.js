c482fbb9e7797a3d92fa664fc82ca50adc599438
unknown <rlcdoloroso@gmail.com>
2023-03-21 11:58:03 +0800

    Update display shows in localhost

--- task1.js
+++ task1.js
@@ -1,11 +1,17 @@
 const http = require('node:http')
-//const fs = require('node:fs')
+const fs = require('node:fs')
 
 const server = http.createServer(
     function handle(req, res) {
-        res.end('muzta')
+        const dis =fs.readFile('./list.txt', 'utf-8', (error1, data1) => {
+            console.log(data1);
+            res.end(data1)
+        })
     }
 )
 
 
-server.listen(8000, '127.0.0.1')
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Server is running.')
+})
