2f4f8482021d245a1998837e6431ebb458ac4d37
unknown <rlcdoloroso@gmail.com>
2023-03-28 14:52:32 +0800

    use pug for html template

--- showlist.js
+++ showlist.js
@@ -2,18 +2,8 @@ const fs = require('node:fs')
 
 function showList() {
     const text = fs.readFileSync("./list.txt", "utf-8");
-    var textByLine = text.split("\n")
+    const textByLine = text.split("\n")
     console.log(textByLine)
-
-    let items = "";
-    for (let i = 0; i < textByLine.length; i++) {
-        items += `<li>${textByLine[i]}</li>`;
-    }
-    return items;
+    return textByLine;
 }
-
-//let show = showList();
-
-//console.log(show);
-
 module.exports = showList;
--- task1.js
+++ task1.js
@@ -1,19 +1,24 @@
 const fs = require('node:fs')
 const showList = require('./showlist');
 
-const express = require("express")
+const express = require('express')
 const server = express()
+const pug = require('pug')
+
+server.set('view engine','pug')
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Server is running.')
 })
 
-server.get("/", (req, res) => {
-    const f = fs.readFileSync('./public/display.html', 'utf-8')
-    res.end(f.replace('{{listahan}}', showList()))
+server.get("/", function(req, res) {
+    res.render(
+        'sample',
+        { title : 'Noice', message : 'List of TV shows', list : showList() }
+    )
 })
 
 server.get("/favicon.ico", (req, res) => {
-    const f3 = fs.readFileSync('./public/darock.jpg')
-    res.end(f3)
+    const f2 = fs.readFileSync('./public/darock.jpg')
+    res.end(f2)
 })
